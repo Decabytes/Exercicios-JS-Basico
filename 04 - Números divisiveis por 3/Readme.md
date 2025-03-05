5d50fe618e16e50e337ac90992ffc79160f57fee
Decabytes <danidev23@gmail.com>
2025-03-05 07:13:34 -0300

    Add files via upload

---
+++ 04 - Números divisiveis por 3/Readme.md
@@ -0,0 +1,11 @@
+# Leitura de pares e ímpares
+
+### Descrição
+
+Elabore um programa que apresente todos os números divisíveis por três que sejam menores que 100.
+
+### Resultado
+
+<p align="center">
+<img src="../11- ImagensReadme/04.gif"  width="570px"/>
+</p>
---
+++ 04 - Números divisiveis por 3/index.js
@@ -0,0 +1,11 @@
+// Elabore um programa que apresente todos os números divisíveis por três que sejam menores que 100.
+
+function main(){
+    var i;
+    for (i=3; i<=100; i=i+3)
+    {
+        console.log(i);
+     }
+}
+console.log('Os números divisíveis por 3 e menores que 100 são:');
+main();
